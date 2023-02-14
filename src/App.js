eb366c414e6cb666f835dc22bba48473deb4cbb0
waikau maina <ericmaina.dev@gmail.com>
2023-02-14 15:47:16 +0300

    set up routing for the react project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./components/home/Home";
 import Navbar from "./components/navbar/Navbar";
+import ProductPage from "./components/products/ProductPage";
 import ProductsList from "./components/products/ProductsList";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Welcome</h1>
-      <ProductsList />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductsList />}>
+          <Route path="/products/:id" element={<ProductPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
