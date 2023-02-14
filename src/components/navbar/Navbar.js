94083f28d5f32c2cec0df10505c4537e5c90586d
waikau maina <ericmaina.dev@gmail.com>
2023-02-14 14:41:44 +0300

    added the relevant icons to the navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,13 +3,16 @@ import Box from "@mui/material/Box";
 import SwipeableDrawer from "@mui/material/SwipeableDrawer";
 import Button from "@mui/material/Button";
 import List from "@mui/material/List";
-import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
+import MenuSharpIcon from "@mui/icons-material/MenuSharp";
+import AccountCircleSharpIcon from "@mui/icons-material/AccountCircleSharp";
+import LoginSharpIcon from "@mui/icons-material/LoginSharp";
+import StoreSharpIcon from "@mui/icons-material/StoreSharp";
+import HomeSharpIcon from "@mui/icons-material/HomeSharp";
 
 export default function Navbar() {
   const [state, setState] = React.useState({
@@ -36,29 +39,38 @@ export default function Navbar() {
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-      <Divider />
-      <List>
-        {["All mail", "Trash", "Spam"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
-        ))}
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <HomeSharpIcon />
+            </ListItemIcon>
+            <ListItemText primary="Home" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <StoreSharpIcon />
+            </ListItemIcon>
+            <ListItemText primary="Products" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <LoginSharpIcon />
+            </ListItemIcon>
+            <ListItemText primary="Log in" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <AccountCircleSharpIcon />
+            </ListItemIcon>
+            <ListItemText primary="Sign Up" />
+          </ListItemButton>
+        </ListItem>
       </List>
     </Box>
   );
@@ -66,7 +78,9 @@ export default function Navbar() {
   return (
     <div>
       <React.Fragment key="left">
-        <Button onClick={toggleDrawer("left", true)}>Left</Button>
+        <Button onClick={toggleDrawer("left", true)}>
+          <MenuSharpIcon onClick={toggleDrawer("left", true)} />
+        </Button>
         <SwipeableDrawer
           anchor="left"
           open={state["left"]}
