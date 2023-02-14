d9372d7997463fb81227f933afc6e0ac9da9daea
waikau maina <ericmaina.dev@gmail.com>
2023-02-14 14:28:39 +0300

    created the navbar and installed icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome</h1>
+      <Navbar />
     </div>
   );
 }
