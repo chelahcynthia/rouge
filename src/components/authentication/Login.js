7fa4eada664ce894a606b7b0829bd198055aa98c
cynthia <cchelangat9892gmail.com>
2023-02-15 14:43:01 +0300

    Created a login form and styled it

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Login.css'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className='login-container'>
+        <div className='brand-title'>Please Login</div>
+        <div className='inputs'>
+            <label>EMAIL</label>
+            <input type="email" placeholder='example@test.com'></input>
+            <label>PASSWORD</label>
+            <input type="password" placeholder='Min 6 characters long'></input>
+            <button type='submit'>LOGIN</button>
+        </div>
+        </div>
   )
 }
 
