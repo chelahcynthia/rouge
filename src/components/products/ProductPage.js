d9c6ea8ed0776cbcff8ef7244748738491108d45
cynthia <cchelangat9892gmail.com>
2023-02-22 09:13:28 +0300

    Started styling individual cards

--- src/components/products/ProductPage.js
+++ src/components/products/ProductPage.js
@@ -1,8 +1,23 @@
 import React from "react";
+import "./ProductPage.css";
 
 function ProductPage() {
   return (
-  <div>ProductPage</div>
+  <div className="page-container">
+  <ul className="card-wrapper">
+    <li className="card">
+      <img src="https://res.cloudinary.com/dphlf7a8o/image/upload/v1676400905/pexels-alesia-kozik-7796985_oe3ah4.jpg" alt="imagecard" />
+      <h1>Skin oil Serum</h1>
+      <p>Lorem ipsum sit dolor amit</p>
+    </li>
+    <li className="card">
+      <img src="https://res.cloudinary.com/dphlf7a8o/image/upload/v1676400905/pexels-alesia-kozik-7796985_oe3ah4.jpg" alt="imagecard" />
+      <h1>Skin oil Serum</h1>
+      <p>Lorem ipsum sit dolor amit</p>
+    </li>
+
+  </ul>
+    </div>
   );
 }
 
