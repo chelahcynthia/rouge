46922ee9d3b13bbcc1bf9796d465a37e417a4bda
cynthia <cchelangat9892gmail.com>
2023-02-22 10:51:48 +0300

    Added button to the card

--- src/components/products/ProductPage.js
+++ src/components/products/ProductPage.js
@@ -9,12 +9,13 @@ function ProductPage() {
       <img src="https://res.cloudinary.com/dphlf7a8o/image/upload/v1676400905/pexels-alesia-kozik-7796985_oe3ah4.jpg" alt="imagecard" />
       <h1>Skin oil Serum</h1>
       <p>Lorem ipsum sit dolor amit</p>
+      <button className="page-btn" type="submit">Buy Now</button>
     </li>
-    <li className="card">
+    {/* <li className="card">
       <img src="https://res.cloudinary.com/dphlf7a8o/image/upload/v1676400905/pexels-alesia-kozik-7796985_oe3ah4.jpg" alt="imagecard" />
       <h1>Skin oil Serum</h1>
       <p>Lorem ipsum sit dolor amit</p>
-    </li>
+    </li> */}
 
   </ul>
     </div>
