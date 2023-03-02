0f0b52c7a0159fab78ee9385bc6d3db0427d1ed3
cynthia <cchelangat9892gmail.com>
2023-03-02 10:57:15 +0300

    Created a login form

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -3,31 +3,26 @@ import "./Login.css";
 
 function Login() {
   return (
-    <div className="login-container">
-      <img
-        src="https://res.cloudinary.com/dfdxhrjcc/image/upload/v1677142774/rouge/misolo_vzrcme.jpg"
-        alt="login"
-        className="login-img"
-      />
-
-      <form className="login-form">
-        <h1 className="form-title">Welcome Back</h1>
-        <div className="form-inputs">
-          <label>Email</label>
-          <br />
-          <input type="email" placeholder="example@test.com"></input>
+    <div className="main">
+      <div className="login-container">
+        <section className="wrapper"></section>
+        <div className="heading">
+          <h1 className="text text-large">LogIn</h1>
+          <p className="text text-normal">
+            New User?{" "}
+            <span>
+              <a href="#" className="text text-links">
+                Create an account
+              </a>
+            </span>
+          </p>
         </div>
-        <div className="form-inputs">
-          <label>Password</label>
-          <br />
-          <input type="password" placeholder="Min 6 characters long"></input>
-        </div>
-        <div className="form-inputs">
-          <button className="login-btn" type="submit">
-            LOGIN
-          </button>
-        </div>
-      </form>
+        <form name="login" className="form">
+          <div className="input-control">
+            <label for="email" className="input-label" hidden>Email Address</label>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
