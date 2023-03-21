5e80529fab402a9bcd733ac10c73ba5ba608bac1
waikau maina <ericmaina.dev@gmail.com>
2023-03-21 12:15:16 +0300

    rouge reboot

--- src/components/authentication/Signup.js
+++ src/components/authentication/Signup.js
@@ -1,27 +1,84 @@
-import React from 'react'
-import './Signup.css'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./Signup.css";
 
 function Signup() {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    username: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+
   return (
-    <div className='signup-container'>
-      <div className='sign-brand'>Create Your Account!</div>
-      <form className='signup-form'>
-        <label>First Name</label>
-        <input className='sign-input' type='text' placeholder='enter your first name'></input>
-        <label>Second Name</label>
-        <input className='sign-input' type='text' placeholder='enter your last name'></input>
-        <label>Email</label>
-        <input className='sign-input' type='email' placeholder='example@test.com'></input>
-        <label>Username</label>
-        <input className='sign-input' type='text' placeholder='enter your username'></input>
-        <label>Password</label>
-        <input className='sign-input' type='password' placeholder='Min 6 characters long'></input>
-        <label>Confirm password</label>
-        <input className='sign-input' type='password' placeholder='Min 6 characters long '></input>
-        <button type='submit' className='signup-btn'>SignUp</button>
+    <div className="signup-container">
+      <div className="sign-brand">Create Your Account!</div>
+      <form className="signup-form" onSubmit={(e) => handleSubmit(e)}>
+        <input
+          className="sign-input"
+          type="text"
+          placeholder="Enter your first name"
+          onChange={(e) =>
+            setFormData({ ...formData, firstName: e.target.value })
+          }
+        ></input>
+        <input
+          className="sign-input"
+          type="text"
+          placeholder="Enter your last name"
+          onChange={(e) =>
+            setFormData({ ...formData, lastName: e.target.value })
+          }
+        ></input>
+        <input
+          className="sign-input"
+          type="email"
+          placeholder="Example@test.com"
+          onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+        ></input>
+        <input
+          className="sign-input"
+          type="text"
+          placeholder="Enter your username"
+          onChange={(e) =>
+            setFormData({ ...formData, username: e.target.value })
+          }
+        ></input>
+        <input
+          className="sign-input"
+          type="password"
+          placeholder="Enter your password"
+          onChange={(e) =>
+            setFormData({ ...formData, password: e.target.value })
+          }
+        ></input>
+        <input
+          className="sign-input"
+          type="password"
+          placeholder="Confirm Password"
+          onChange={(e) =>
+            setFormData({ ...formData, confirmPassword: e.target.value })
+          }
+        ></input>
+        <button type="submit" className="signup-btn">
+          SignUp
+        </button>
       </form>
-      </div>
-  )
+      <p className="redirect-link">
+        Already have an account?{" "}
+        <Link className="rlink" to="/login">
+          Login here
+        </Link>
+      </p>
+    </div>
+  );
 }
 
-export default Signup
+export default Signup;
