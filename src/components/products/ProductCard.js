7e897ed0c8d4316a056286b2ddfa89813bb8fbc9
cynthia <cchelangat9892gmail.com>
2023-03-22 09:30:25 +0300

    Created an image div in the card containe r

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -4,11 +4,12 @@ import "./ProductCard.css";
 function ProductCard() {
   return (
     <div className="product-card">
-      <img
-        className="product-image"
-        src="https://res.cloudinary.com/dfdxhrjcc/image/upload/v1676377741/rouge/pexels-alesia-kozik-7796597_vy697y.jpg"
-        alt="dropper"
-      />
+      <div className="product-image">
+        <img
+          src="https://res.cloudinary.com/dfdxhrjcc/image/upload/v1676377741/rouge/pexels-alesia-kozik-7796597_vy697y.jpg"
+          alt="dropper"
+        />
+      </div>
       <span className="product-title">product-title</span>
       <span className="product-price">Kes. 399</span>
     </div>
